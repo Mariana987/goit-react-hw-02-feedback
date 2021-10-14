0f8e5deca9353125874a0819c4c13f652bf3d5c2
Mariana987 <m1a1r1y1a1n1a1@gmail.com>
2021-10-14 18:11:21 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import Statistics from './components/Statistics';
+import FeedbackOptions from './components/FeedbackOptions';
+import Section from './components/Section';
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = option => {
+    this.setState(prevStat => ({
+      [option]: prevStat[option] + 1,
+    }));
+  };
+
+  countTotalFeedbacks = () => this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = totalFeedbacks =>
+    Math.round((this.state.good / totalFeedbacks) * 100);
+  render() {
+    const { good, neutral, bad } = this.state;
+    const { handleIncrement, countTotalFeedbacks, countPositiveFeedbackPercentage } = this;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={handleIncrement} />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            countTotalFeedbacks={countTotalFeedbacks}
+            countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+          />
+        </Section>
+      </>
+    );
+  }
 }
-
 export default App;
